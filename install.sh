26ed21d396d956aa4b174e0bb07603155e778372
Viacheslav Popika <popikeyshen@gmail.com>
2018-08-13 12:55:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@
 #       https://docs.opencv.org/3.4/d7/d9f/tutorial_linux_install.html
 ############################################################################
 cd
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 git clone https://github.com/opencv/opencv
 #git clone https://github.com/opencv/opencv_contrib.git
 cd ~/opencv
