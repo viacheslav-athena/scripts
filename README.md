f6585b791ae1b41da9ece53562cf7b5ccc1fdbfa
popikeyshen <popikeyshen@gmail.com>
2018-08-13 12:51:35 +0300

    first script

---
+++ README.md
@@ -0,0 +1,20 @@
+#This my autoinstallation scripts
+
+For some time, I installed libraries for my embedded systems manually from the command line. It takes more time than the autoinstallation script or docker. In this repository I will add more scripts for autoinstallation. If someone uses this repository, but he lacks some packages - write and I will add.
+
+## List of packages
+
+1. Opencv
+2. Dlib
+3. Eigen
+
+### Installing
+
+
+```
+sudo bash ./install.sh
+```
+
+## Authors
+
+Viacheslav Popika
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#sudo bash path/to/install.sh
+#chmod +x install.sh
+
+############################################################################
+# 1) Install Opencv 
+#       https://docs.opencv.org/3.4/d7/d9f/tutorial_linux_install.html
+############################################################################
+cd
+sudo apt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+git clone https://github.com/opencv/opencv
+#git clone https://github.com/opencv/opencv_contrib.git
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j7
+sudo make install
+
+############################################################################
+# 2) Install Dlib
+############################################################################
+cd
+git clone https://github.com/davisking/dlib
+cd dlib
+#./install.sh
+mkdir build
+cd build
+cmake ..
+sudo make install
+
+############################################################################
+# 3) Install Eigen
+############################################################################
+cd 
+git clone https://github.com/libigl/eigen
+cd eigen
+mkdir build
+cd build
+cmake ..
+sudo make install
+
+
+
